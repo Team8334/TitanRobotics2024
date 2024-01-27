5ccb2eda04caeea31448f9d460724b50743517ae
177224 <trev23bev@gmail.com>
2024-01-27 10:42:38 -0700

    sets up drive base (ready for input)

--- TitanRobotics2024/src/main/java/frc/robot/Subsystem/DriveBase.java
+++ TitanRobotics2024/src/main/java/frc/robot/Subsystem/DriveBase.java
@@ -2,51 +2,45 @@ package frc.robot.Subsystem;
 
 import frc.robot.Data.PortMap;
 
-public class DriveBase implements Subsystem 
-{
+public class DriveBase implements Subsystem {
 
   @Override
-  public void start() 
-  {
+  public void start() {
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'start'");
   }
 
-  public double frontleft;
-  public double frontright;
-  public double backleft;
-  public double backright;
+  public double frontLeft;
+  public double frontRight;
+  public double backLeft;
+  public double backRight;
 
-  private final ModifiedMotors motorFrontLeft;
-  private final ModifiedMotors motorRearLeft;
-  private final ModifiedMotors motorFrontRight;
-  private final ModifiedMotors motorRearRight;
+  private ModifiedMotors motorRearLeft;
+  private ModifiedMotors motorFrontRight;
+  private ModifiedMotors motorRearRight;
+  private ModifiedMotors motorFrontLeft;
 
   private static DriveBase instance = null;
 
-  public static DriveBase getInstance() 
-  {
+  public static DriveBase getInstance() {
     if (instance == null) {
       instance = new DriveBase();
     }
     return instance;
   }
 
-  public DriveBase() 
-  {
-    motorFrontLeft = new ModifiedMotors(PortMap.FRONTLEFT.portNumber);
-    motorRearLeft = new ModifiedMotors(PortMap.REARLEFT.portNumber);
-    motorFrontRight = new ModifiedMotors(PortMap.FRONTRIGHT.portNumber);
-    motorRearRight = new ModifiedMotors(PortMap.REARRIGHT.portNumber);
+  public DriveBase() {
+    this.motorFrontLeft = new ModifiedMotors(PortMap.FRONTLEFT.portNumber);
+    this.motorRearLeft = new ModifiedMotors(PortMap.REARLEFT.portNumber);
+    this.motorFrontRight = new ModifiedMotors(PortMap.FRONTRIGHT.portNumber);
+    this.motorRearRight = new ModifiedMotors(PortMap.REARRIGHT.portNumber);
   }
 
-  public void setRightSideMotors(double power) 
-  {
+  public void setRightSideMotors(double power) {
     // set the right side motors to "power"
   }
 
-  public void setLeftSideMotors(double power) 
-  {
+  public void setLeftSideMotors(double power) {
     // set the left side motors to "power"
   }
 
@@ -54,19 +48,21 @@ public class DriveBase implements Subsystem
    * Control Type: Left Stick controls speed; Right Stick controls direction (Used
    * in tank)
    */
-  public void drive(double forward, double turn) 
-  {
+  public void drive(double forward, double turn) {
+    this.frontLeft = (-forward + (0.35 * turn));
+    this.frontRight = (-forward + (0.35 * turn));
+    this.backLeft = (-forward + (0.35 * turn));
+    this.backRight = (-forward + (0.35 * turn));
     // put code here that drives the bot using the inputs "forward" and "turn"
   }
 
   @Override
   /* Updates the state the motors are in */
-  public void update() 
-  {
-    motorFrontLeft.set(0); // 0 is a placeholder
-    motorRearLeft.set(0);
-    motorFrontRight.set(0);
-    motorRearRight.set(0);
+  public void update() {
+    this.motorFrontLeft.set(frontLeft); // 0 is a placeholder
+    this.motorRearLeft.set(0);
+    this.motorFrontRight.set(0);
+    this.motorRearRight.set(0);
     // this.driveSave.frontleft
   }
 }
--- TitanRobotics2024/src/main/java/frc/robot/Subsystem/ModifiedMotors.java
+++ TitanRobotics2024/src/main/java/frc/robot/Subsystem/ModifiedMotors.java
@@ -6,7 +6,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ModifiedMotors implements Subsystem {
 
-
     @Override
     public void start() {
         // TODO Auto-generated method stub
@@ -18,20 +17,16 @@ public class ModifiedMotors implements Subsystem {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
-    
-     private int portNumber;
+
+    private int portNumber;
     private final MotorController motor;
-    
-    public ModifiedMotors(int portNumber) 
-    {
+
+    public ModifiedMotors(int portNumber) {
         this.portNumber = portNumber;
         MotorController motorTemporarily;
-        try 
-        {
+        try {
             motorTemporarily = new PWMVictorSPX(portNumber);
-        }
-        catch (Exception motorNotIdenitfied) 
-        {
+        } catch (Exception motorNotIdenitfied) {
             motorTemporarily = null;
             SmartDashboard.putNumber("Error: Port Not Activated", this.portNumber);
         }
