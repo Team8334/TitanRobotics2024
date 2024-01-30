7683969f0196b6039f91197f4f69e550e786d747
rsneller <rhea.sneller@gmail.com>
2024-01-30 15:31:45 -0700

    Create Ramp.java

---
+++ TitanRobotics2024/src/main/java/frc/robot/Subsystem/Ramp.java
@@ -0,0 +1,19 @@
+package frc.robot.Subsystem;
+
+public class Ramp implements Subsystem
+{
+    private static Ramp instance = null;
+    
+    @Override
+    public void start()
+    {
+
+    }
+    
+    @Override
+    public void update()
+    {
+
+    }
+
+}
