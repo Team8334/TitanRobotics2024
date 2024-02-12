32b47eb72d5eadc3144c302ca56cfa55f6c32747
thebuilder6 <43260133+thebuilder6@users.noreply.github.com>
2024-02-12 16:02:53 -0700

    Squashed commit of the following:
    commit 9e63928499f27a0bb5dfd7d65537621dc1eb6f74
    Author: 177224 <trev23bev@gmail.com>
    Date:   Sat Jan 27 11:35:29 2024 -0700
    
        updated terms and made code shorter
    
    commit 253c11d69b9c8738db7979c2223c94f97d2ecaa8
    Author: 177224 <trev23bev@gmail.com>
    Date:   Sat Jan 27 11:20:49 2024 -0700
    
        somewhat fixes things
    
        got the left side inverted via negatives in math portion of code.
    
    commit 5ccb2eda04caeea31448f9d460724b50743517ae
    Author: 177224 <trev23bev@gmail.com>
    Date:   Sat Jan 27 10:42:38 2024 -0700
    
        sets up drive base (ready for input)

--- TitanRobotics2024/src/main/java/frc/robot/Subsystem/DriveBase.java
+++ TitanRobotics2024/src/main/java/frc/robot/Subsystem/DriveBase.java
@@ -2,60 +2,65 @@ package frc.robot.Subsystem;
 
 import frc.robot.Data.PortMap;
 
-public class DriveBase implements Subsystem 
-{
+public class DriveBase implements Subsystem {
 
+  @Override
+  public void start() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'start'");
+  }
 
   private double leftPower;
   private double rightPower;
 
-  private ModifiedMotors rearLeftMotor;
-  private ModifiedMotors frontRightMotor;
-  private ModifiedMotors rearRightMotor;
-  private ModifiedMotors frontLeftMotor;
+  private ModifiedMotors motorRearLeft;
+  private ModifiedMotors motorFrontRight;
+  private ModifiedMotors motorRearRight;
+  private ModifiedMotors motorFrontLeft;
 
   private static DriveBase instance = null;
 
-  public static DriveBase getInstance() 
-  {
-    if (instance == null) 
-    {
+  public static DriveBase getInstance() {
+    if (instance == null) {
       instance = new DriveBase();
     }
     return instance;
   }
 
-  public DriveBase() 
-  {
-    this.frontLeftMotor  =  new ModifiedMotors(PortMap.FRONTLEFT.portNumber, "CANVictorSPX");
-    this.rearLeftMotor   =  new ModifiedMotors(PortMap.REARLEFT.portNumber, "CANVictorSPX");
-    this.frontRightMotor =  new ModifiedMotors(PortMap.FRONTRIGHT.portNumber, "CANVictorSPX");
-    this.rearRightMotor  =  new ModifiedMotors(PortMap.REARRIGHT.portNumber, "CANVictorSPX");
+  public DriveBase() {
+    this.motorFrontLeft  =  new ModifiedMotors(PortMap.FRONTLEFT.portNumber);
+    this.motorRearLeft   =  new ModifiedMotors(PortMap.REARLEFT.portNumber);
+    this.motorFrontRight =  new ModifiedMotors(PortMap.FRONTRIGHT.portNumber);
+    this.motorRearRight  =  new ModifiedMotors(PortMap.REARRIGHT.portNumber);
+
   }
 
-  public void setRightMotorsPower(double power) 
-  {
+  public void setRightSideMotors(double power) {
     this.rightPower = power;
   }
 
-  public void setLeftMotorsPower(double power) 
-  {
+  public void setLeftSideMotors(double power) {
     this.leftPower = power;
   }
 
-  public void drive(double forward, double turn) 
-  {
-    this.leftPower = (forward - (0.35 * turn));
+  /**
+   * Control Type: Left Stick controls speed; Right Stick controls direction (Used
+   * in tank)
+   */
+  public void drive(double forward, double turn) {
+    this.leftPower = (forward - (0.35 * turn));// invert using negetive for left side motors
     this.rightPower = (forward + (0.35 * turn));
+  
+    // put code here that drives the bot using the inputs "forward" and "turn"
   }
 
   @Override
   /* Updates the state the motors are in */
-  public void update() 
-  {
-    this.frontLeftMotor.set(leftPower); 
-    this.rearLeftMotor.set(leftPower);
-    this.frontRightMotor.set(-rightPower);
-    this.rearRightMotor.set(-rightPower);
+  public void update() {
+    this.motorFrontLeft.set(-leftPower); // 0 is a placeholder
+    this.motorRearLeft.set(-leftPower);
+    this.motorFrontRight.set(rightPower);
+    this.motorRearRight.set(rightPower);
+    // this.driveSave.frontleft
   }
 }
