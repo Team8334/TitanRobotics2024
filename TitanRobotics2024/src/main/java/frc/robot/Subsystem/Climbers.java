bb5da41f5bc4bb1ef669534745f9861b013454fe
thebuilder6 <43260133+thebuilder6@users.noreply.github.com>
2024-02-12 19:07:10 -0700

    Austin Cromar
    made TOP BOTTOM and HOLD

--- TitanRobotics2024/src/main/java/frc/robot/Subsystem/Climbers.java
+++ TitanRobotics2024/src/main/java/frc/robot/Subsystem/Climbers.java
@@ -1,65 +1,95 @@
 package frc.robot.Subsystem;
 
+import edu.wpi.first.math.controller.PIDController;
 import frc.robot.Data.PortMap;
 
 public class Climbers {
-    private double power;
-    private ModifiedMotors motor;
-    private String climberState = "retracted";
-    
-    private static Climbers instanceLeft = null;
-    private static Climbers instanceRight = null;
-    private PIDController PID;
-    public static Climbers getInstanceLeft() {
-      if (instanceLeft == null)
-      {
-        instanceLeft = new Climbers(new ModifiedMotors(PortMap.CLIMBMOTORLEFT.portNumber, "CANVictorSPX"));
-      }
-        return instanceLeft;
-    }
-    public static Climbers getInstanceRight()
-    {
-      if (instanceRight == null)
-      {
-        instanceRight = new Climbers( new ModifiedMotors(PortMap.CLIMBMOTORRIGHT.portNumber, "CANVictorSPX"));
-      }
-        return instanceRight;
-    }
+  private double power;
+  private ModifiedMotors motor;
+  private String climberState = "retracted";
 
-    public Climbers(ModifiedMotors motor, Encoder encoder) 
+  private static Climbers instanceLeft = null;
+  private static Climbers instanceRight = null;
+  private PIDController PID;
+
+  private double encoderPosition = 0;
+  private double encoderRate = 0;
+  private double holdPosition;
+
+  private double targetPosition = 0;
+
+  private double TOP_POSITION = 0;
+  private double BOTTOM_POSITION = 0;
+  
+
+  public static Climbers getInstanceLeft() {
+    if (instanceLeft == null)
     {
-      this.motor = motor
-      this.encoder = encoder
-      this.PID = new PIDController
+      instanceLeft =
+          new Climbers(new ModifiedMotors(PortMap.CLIMBMOTORLEFT.portNumber, "CANVictorSPX"),
+              new Encoder(PortMap.CLIMBMOTORLEFT.portNumber, PortMap.CLIMBMOTORLEFT.portNumber));
     }
-    public void top()
+    return instanceLeft;
+  }
+
+  public static Climbers getInstanceRight() {
+    if (instanceRight == null)
     {
-      climberState = "TOP"
+      instanceRight =
+          new Climbers(new ModifiedMotors(PortMap.CLIMBMOTORRIGHT.portNumber, "CANVictorSPX"),
+              new Encoder(PortMap.CLIMBMOTORRIGHT.portNumber, PortMap.CLIMBMOTORRIGHT.portNumber));
     }
-    public void bottom()
-    {
-      climberState = "BOTTOM"
-    }
-    public void manual()
-    {
-      climberState = "MANUAL"
-    }
-    private void processedState()
+    return instanceRight;
+  }
+
+  public Climbers(ModifiedMotors motor, Encoder encoder) {
+    this.motor = motor;
+    this.encoder = encoder;
+    this.PID = new PIDController(1, 0, 0);
+  }
+
+  public void top() {
+    climberState = "TOP";
+  }
+
+  public void bottom() {
+    climberState = "BOTTOM";
+  }
+
+  public void hold() {
+    climberState = "HOLD";
+    holdPosition = encoderPosition;
+  }
+
+  public void manual() {
+    climberState = "MANUAL";
+  }
+
+  private void processedState()
     {
       switch (climberState){
         case "TOP":
+          targetPosition = TOP_POSITION;
           break;
         case "BOTTOM":
+          targetPosition = BOTTOM_POSITION;
           break;
         case "HOLD":
+          targetPosition = holdPosition;
           break;
         case "MANUAL":
           break;
         default:
           break;
+          
       }
-    }      
+      power = PID.calculate(encoderPosition, targetPosition);
+    }
+
   public void update() {
-        motor.set(power); // 0 is a placeholder
-      }
+    encoderRate = encoder.getRate();
+    encoderPosition = encoder.getDistance();
+    processedState();
+    motor.set(power); // 0 is a placeholder
+  }
 }
