4ec27629fd7b93b1dbe411e57271dfb528193097
impossiblerocket <46904736+impossiblerocket@users.noreply.github.com>
2024-02-24 09:45:31 -0700

    Update LeaveCommunityRightMission.java

--- TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/LeaveCommunityRightMission.java
+++ TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/LeaveCommunityRightMission.java
@@ -1,6 +1,7 @@
 package frc.robot.Auto.Missions;
 
 import frc.robot.Auto.AutoMissionEndedException;
+import frc.robot.Auto.Actions.DriveForDistanceAction;
 
 
 public class LeaveCommunityRightMission extends MissionBase 
@@ -8,6 +9,7 @@ public class LeaveCommunityRightMission extends MissionBase
     @Override
     protected void routine() throws AutoMissionEndedException 
     {
+        runAction(new DriveForDistanceAction(60, 6));
         System.out.println("Leave Community Right Mission");
     }
 }
