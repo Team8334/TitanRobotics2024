dc53315f702454bcc88ff819fe09c9edb5cff44d
impossiblerocket <46904736+impossiblerocket@users.noreply.github.com>
2024-03-05 15:33:38 -0700

    Update OneNoteMission.java

--- TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/OneNoteMission.java
+++ TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/OneNoteMission.java
@@ -3,6 +3,7 @@ package frc.robot.Auto.Missions;
 import frc.robot.Auto.AutoMissionEndedException;
 import frc.robot.Auto.Actions.DriveForTimeAction;
 import frc.robot.Auto.Actions.ParallelAction;
+import frc.robot.Auto.Actions.RunScoringSystemAction;
 import frc.robot.Auto.Actions.TurnDegreesAction;
 import frc.robot.Auto.Actions.SeriesAction;
 
@@ -11,6 +12,8 @@ public class OneNoteMission extends MissionBase
     @Override
     protected void routine() throws AutoMissionEndedException
     {
-        runAction(new DriveForTimeAction(3, 0.5));
+        runAction(new ParallelAction(new DriveForTimeAction(2, 0.5), new RunScoringSystemAction(2, 0, 0, 0.3)));
+        runAction(new RunScoringSystemAction(3, 0, 0.5, 0.6));
+        runAction(new ParallelAction(new RunScoringSystemAction(2, 0, 0, 0.3), new DriveForTimeAction(2, -0.5)));
     }
 }
