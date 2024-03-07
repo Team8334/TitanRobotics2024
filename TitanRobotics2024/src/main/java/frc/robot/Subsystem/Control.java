6e9685144151ddbc522859d0a1ad637745683eb3
177224 <trev23bev@gmail.com>
2024-03-06 18:42:39 -0700

    makes comments more descriptive

--- TitanRobotics2024/src/main/java/frc/robot/Subsystem/Control.java
+++ TitanRobotics2024/src/main/java/frc/robot/Subsystem/Control.java
@@ -135,15 +135,15 @@ public class Control implements Subsystem
 
     }
 
-    private void manipulatorControl()//pls do not mess with the buttons, they are set to operator's preference.
+    private void manipulatorControl()//please do not mess with the buttons, they are set to operator's preference.
     {
-        if (operatorController.getButton(ButtonMap.XboxRIGHTBumper))//right bumper = intake in
+        if (operatorController.getButton(ButtonMap.XboxRIGHTBumper))//right bumper = intake in, pushes ramp back towards the intake
         {
             intake.manualIntakePower(0.3);
             intake.manualPivotPower(0);
             ramp.setRamp(-0.3);
         }
-        else if (operatorController.getButton(ButtonMap.XboxLEFTBumper))//left bumper = intake out
+        else if (operatorController.getButton(ButtonMap.XboxLEFTBumper))//left bumper = intake out, pushes ramp towards the scoring side
         {
             intake.manualIntakePower(-0.3);
             intake.manualPivotPower(0);
