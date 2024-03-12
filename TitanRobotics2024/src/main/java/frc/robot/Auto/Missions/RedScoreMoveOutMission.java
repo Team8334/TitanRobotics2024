43fe82c6d3232a802cc66ec447c754a6a9fba2f6
aadams235 <122327119+aadams235@users.noreply.github.com>
2024-03-12 16:52:08 -0600

    Slew Drive

--- TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/RedScoreMoveOutMission.java
+++ TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/RedScoreMoveOutMission.java
@@ -7,7 +7,6 @@ import frc.robot.Auto.Actions.TurnDegreesAction;
 
 public class RedScoreMoveOutMission extends MissionBase
 {
-    
     @Override
     protected void routine() throws AutoMissionEndedException 
     // should drive forward, intake on front, activate the intake, and then we can make in do other stuff
@@ -15,7 +14,7 @@ public class RedScoreMoveOutMission extends MissionBase
         runAction(new DriveForDistanceAction(1.7, 3));  
         runAction(new TurnDegreesAction(-77, 1));
         runAction(new DriveForDistanceAction(1.5, 3));
-        //runAction(new RunningScoringActions(5, .25, .25, .25));
+        runAction(new RunningScoringActions(5, .25, .25, .25));
         runAction(new DriveForDistanceAction(-1.5, 3));
         runAction(new TurnDegreesAction(90, 1));
         runAction(new DriveForDistanceAction(0, 2));
