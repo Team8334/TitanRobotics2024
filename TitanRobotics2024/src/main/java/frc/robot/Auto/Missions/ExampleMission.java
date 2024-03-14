9ebc67c149f21cecc41037182d84e54f45c5f1ba
impossiblerocket <46904736+impossiblerocket@users.noreply.github.com>
2024-03-13 18:11:06 -0600

    this is fine

--- TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/ExampleMission.java
+++ TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/ExampleMission.java
@@ -6,6 +6,7 @@ import frc.robot.Auto.Actions.DriveForTimeAction;
 import frc.robot.Auto.Actions.LockOnAction;
 import frc.robot.Auto.Actions.ParallelAction;
 import frc.robot.Auto.Actions.RunScoringSystemAction;
+import frc.robot.Auto.Actions.ScoringSystemStateAction;
 import frc.robot.Auto.Actions.TurnDegreesAction;
 import frc.robot.Auto.Actions.SeriesAction;
 
@@ -14,7 +15,9 @@ public class ExampleMission extends MissionBase
     @Override
     protected void routine() throws AutoMissionEndedException 
     {
-        runAction(new RunScoringSystemAction(3, 0, 0, 0.6));  
+        runAction(new DriveForDistanceAction(40, 3.0));
+        runAction(new TurnDegreesAction(90.0, 3.0));
+        runAction(new ScoringSystemStateAction(3.0, "score piece"));
     }
 }
 
