bd018053e1bec78f8f4986b96c8ac050798af53e
177224 <trev23bev@gmail.com>
2024-03-19 16:36:52 -0600

    makes it (hopefully) so it will decide which side the robot has started on and will run that auto.
    untested, but committing so it can be tested by anyone. uses the FMS to get the alliance, then uses that input to decide if it should run the red side of Scoring mission or the blue side of scoring mission.

--- TitanRobotics2024/src/main/java/frc/robot/Auto/AutoMissionChooser.java
+++ TitanRobotics2024/src/main/java/frc/robot/Auto/AutoMissionChooser.java
@@ -3,24 +3,26 @@ package frc.robot.Auto;
 import frc.robot.Auto.Missions.*;
 import java.util.Optional;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DriverStation;
 
-public class AutoMissionChooser 
+public class AutoMissionChooser
 {
-    enum DesiredMission 
+    enum DesiredMission
     {
         doNothing,
         leaveCommunityRight,
         exampleMission,
         oneNoteMission,
         TwoNoteMission,
-        ScoringMission,
         ScoringThenMovingMission,
         RedScorePickUpMission,
         BlueScorePickUpMission,
         RedScoreMoveOutMission,
-        BlueScoreMoveOutMission
+        BlueScoreMoveOutMission,
+        ScoringMission
     }
 
     private DesiredMission cachedDesiredMission = DesiredMission.doNothing;
@@ -29,7 +31,9 @@ public class AutoMissionChooser
 
     private Optional<MissionBase> autoMission = Optional.empty();
 
-    public AutoMissionChooser() 
+    String alliance;
+
+    public AutoMissionChooser()
     {
         missionChooser = new SendableChooser<>();
 
@@ -39,40 +43,50 @@ public class AutoMissionChooser
         missionChooser.addOption("Score 1 note in amp", DesiredMission.oneNoteMission);
         missionChooser.addOption("Score 2 notes in amp", DesiredMission.TwoNoteMission);
 
-        missionChooser.addOption("Scoring 1 note", DesiredMission.ScoringMission);
+        missionChooser.addOption("Scoring 1 note but red", DesiredMission.ScoringMission);
         missionChooser.addOption("Score and Move After", DesiredMission.ScoringThenMovingMission);
         missionChooser.addOption("Red Score and pick up", DesiredMission.RedScorePickUpMission);
         missionChooser.addOption("Blue Score and pick up", DesiredMission.BlueScorePickUpMission);
-        missionChooser.addOption("Red Score and move out", DesiredMission.RedScoreMoveOutMission);
-        missionChooser.addOption("Blue Score and move out", DesiredMission.BlueScoreMoveOutMission);
+        //  missionChooser.addOption("Red Score and move out", DesiredMission.RedScoreMoveOutMission); //these options do work, they are commented out to not confuse the drivers. 
+        //  missionChooser.addOption("Blue Score and move out", DesiredMission.BlueScoreMoveOutMission);  //they are left in because they are helpful in the case we lose the abliltiy to use limelights.
 
         // add more here as needed
 
         SmartDashboard.putData("Auto Mission", missionChooser);
-        SmartDashboard.putString( "Current Action System", "None");
+        SmartDashboard.putString("Current Action System", "None");
+
+        try
+        {
+            alliance = DriverStation.getAlliance().orElseThrow(() -> new Exception("No alliance")).toString();
+        }
+        catch (Exception e)
+        {
+            // Handle the exception, for example:
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
     }
 
-    public void updateMissionCreator() 
+    public void updateMissionCreator()
     {
         DesiredMission desiredMission = missionChooser.getSelected();
 
-        if (desiredMission == null) 
+        if (desiredMission == null)
         {
             desiredMission = DesiredMission.doNothing;
         }
 
-        if (cachedDesiredMission != desiredMission) 
+        if (cachedDesiredMission != desiredMission)
         {
             System.out.println("Auto selection changed, updating creator: desiredMission->" + desiredMission.name());
             autoMission = getAutoMissionForParams(desiredMission);
         }
-        
+
         cachedDesiredMission = desiredMission;
     }
 
-     private Optional<MissionBase> getAutoMissionForParams(DesiredMission mission) 
-     {
-        switch (mission) 
+    private Optional<MissionBase> getAutoMissionForParams(DesiredMission mission)
+    {
+        switch (mission)
         {
             case doNothing:
                 return Optional.of(new DoNothingMission());
@@ -80,22 +94,33 @@ public class AutoMissionChooser
                 return Optional.of(new LeaveCommunityRightMission());
             case exampleMission:
                 return Optional.of(new ExampleMission());
-            case oneNoteMission:
-                return Optional.of(new OneNoteMission());
+            // case oneNoteMission:
+            //     return Optional.of(new OneNoteMission());
             case TwoNoteMission:
                 return Optional.of(new TwoNoteMission());
             case ScoringMission:
-                return Optional.of(new ScoringMission());
+                if (alliance == "Red")
+                {
+                    return Optional.of(new RedScoringMission());
+                }
+                else if (alliance == "Blue")
+                {
+                    return Optional.of(new BlueScoringMission());
+                }
+                else
+                {
+                    return Optional.of(new DoNothingMission());
+                }
             case ScoringThenMovingMission:
                 return Optional.of(new ScoringThenMovingMission());
-            case RedScorePickUpMission:
-                return Optional.of(new RedScorePickUpMission());
-            case BlueScorePickUpMission:
-                return Optional.of(new BlueScorePickUpMission());
-            case RedScoreMoveOutMission:
-                return Optional.of(new RedScoreMoveOutMission());
-            case BlueScoreMoveOutMission:
-                return Optional.of(new BlueScoreMoveOutMission());
+            //case RedScorePickUpMission:
+            //    return Optional.of(new RedScorePickUpMission());
+            //case BlueScorePickUpMission:
+            //    return Optional.of(new BlueScorePickUpMission());
+            //case RedScoreMoveOutMission:
+            //    return Optional.of(new RedScoreMoveOutMission());
+            //case BlueScoreMoveOutMission:
+            //    return Optional.of(new BlueScoreMoveOutMission());
 
             default:
                 System.err.println("No valid autonomous mission found for" + mission);
@@ -103,18 +128,18 @@ public class AutoMissionChooser
         }
     }
 
-    public void reset() 
+    public void reset()
     {
         autoMission = Optional.empty();
         cachedDesiredMission = DesiredMission.doNothing;
     }
-    
-    public void outputToSmartDashboard() 
+
+    public void outputToSmartDashboard()
     {
         SmartDashboard.putString("AutoMissionSelected", cachedDesiredMission.name());
     }
 
-    public Optional<MissionBase> getAutoMission() 
+    public Optional<MissionBase> getAutoMission()
     {
         return autoMission;
     }
--- TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/BlueScoringMission.java
+++ TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/BlueScoringMission.java
@@ -8,23 +8,23 @@ import frc.robot.Auto.Actions.ParallelAction;
 import frc.robot.Auto.Actions.RunScoringSystemAction;
 import frc.robot.Auto.Actions.TurnDegreesAction;
 import frc.robot.Auto.Actions.RunningScoringActions;
-import frc.robot.Auto.Actions.ScoringSystemStateAction;;
+import frc.robot.Auto.Actions.ScoringSystemStateAction;
 
-//none of the number values are for certain, they are just placeholders
+public class BlueScoringMission extends MissionBase {
 
-public class ScoringMission extends MissionBase
-{
-     @Override
-    protected void routine() throws AutoMissionEndedException 
-    // should work from alliance wall, might need to be slightly adjusted for aiode, distance measurements are in meters
+    @Override
+    protected void routine() throws AutoMissionEndedException
     {
         runAction(new DriveForDistanceAction(-0.9, 3));  
         //runAction(new DriveForTimeAction(1, 0.5) needed?
-        runAction(new TurnDegreesAction(-77, 3));
+        runAction(new TurnDegreesAction(77, 3));
         //runAction(new ParallelAction(new DriveForDistanceAction(-0.7, 3), new RunScoringSystemAction(3.0,0.0,0.0,0.6)));
         runAction(new BackLockOnAction("Amp", true, 3));
         runAction(new ParallelAction(new ScoringSystemStateAction(2.5, "score piece"), new DriveForTimeAction(2.5, -0.5)));
         runAction(new ParallelAction(new DriveForTimeAction(1.0, 0.5), new ScoringSystemStateAction(1.0, "score piece")));
-    }
-}
 
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'routine'");
+    }
+    
+}
