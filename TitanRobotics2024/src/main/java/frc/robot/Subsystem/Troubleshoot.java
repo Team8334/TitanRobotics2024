6ffe9d7e77a0f47fc2fda6cac93bf8f375ebe6ab
BismuthTime <122326994+echo2334@users.noreply.github.com>
2024-09-17 16:47:39 -0600

    created troubleshoot.java to explore the issue of encoders disabling drive base when unplugged

---
+++ TitanRobotics2024/src/main/java/frc/robot/Subsystem/Troubleshoot.java
@@ -0,0 +1,50 @@
+package frc.robot.Subsystem;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import frc.robot.Data.PortMap;
+
+public class Troubleshoot implements Subsystem {
+
+    private static Troubleshoot instance = null;
+
+    private ModifiedMotors testMotor;
+    private ModifiedEncoders testEncoder;
+
+    public static Troubleshoot getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Troubleshoot();
+        }
+        return instance;
+    }
+
+    public Troubleshoot()
+    {
+        this. testMotor = new ModifiedMotors(PortMap.FRONTLEFT.portNumber, "CANSparkMax");
+        this. testEncoder = new ModifiedEncoders(PortMap.LEFTENCODER_A.portNumber, PortMap.LEFTENCODER_B.portNumber,"E4TEncoder");
+
+    }
+
+    private Troubleshoot(ModifiedMotors Tmotor)
+    {
+      this.testMotor = Tmotor;
+    }
+
+
+
+    @Override
+    public void update()
+    {
+        testMotor.set(0.2);
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+    
+}
