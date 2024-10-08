4942307eb14beb2615c4692ace81fe934cd36b5b
BismuthTime <94087317+VannyTheGryphon@users.noreply.github.com>
2024-10-08 16:31:32 -0600

    Blue auto*
    should all work, blue and red not opposites of each other

--- TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/BlueScoreMoveOutMission.java
+++ TitanRobotics2024/src/main/java/frc/robot/Auto/Missions/BlueScoreMoveOutMission.java
@@ -15,14 +15,16 @@ public class BlueScoreMoveOutMission extends MissionBase
     @Override
     protected void routine() throws AutoMissionEndedException 
     {      
-	runAction(new WaitAction(AutoMissionChooser.delay));
-        runAction(new DriveForDistanceAction(-0.50, 2.5));  
-        runAction(new TurnDegreesAction(75.0, 2));
-        //runAction(new BackLockOnAction("Amp", true, 2));
-        runAction(new DriveForTimeAction(0.5, -0.5));
-        runAction(new ParallelAction(new DriveForTimeAction(2.0, -0.22), new ScoringSystemStateAction(2.0, "score piece")));
-        runAction(new ParallelAction(new DriveForDistanceAction(0.35, 1.5)  , new ScoringSystemStateAction(1.0, "score piece")));
-        runAction(new TurnDegreesAction(-70.0, 3));
+	  runAction(new WaitAction(AutoMissionChooser.delay));
+    runAction(new WaitAction(AutoMissionChooser.delay));
+    runAction(new DriveForDistanceAction(-0.53, 3));  
+    runAction(new TurnDegreesAction(75, 2));
+    //runAction(new BackLockOnAction("Amp", true, 2));
+    runAction(new DriveForTimeAction(0.38, -0.25));
+    runAction(new ScoringSystemStateAction(2.5, "score piece"));
+    runAction(new ScoringSystemStateAction(1.0, "score piece"));
+      runAction(new DriveForDistanceAction(0.5, 1));
+    runAction(new TurnDegreesAction(-70.0, 3));
       runAction(new DriveForDistanceAction(-0.40, 2));
     }
 }
